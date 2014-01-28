28ff5776fc02436c6b25f5b8fe4ab74231d996c5
looderb <libotte@e-teach.ch>
2014-01-28 15:05:58 +0100

    DA

--- WebApplication1/_DataAccess.cs
+++ WebApplication1/_DataAccess.cs
@@ -12,7 +12,11 @@ namespace WebApplication1
             : base(context, "_DataAccess", "ConnectionString")
         {
         }
-
+        /// <summary>
+        /// Donne le détail d'une personne
+        /// </summary>
+        /// <param name="dataSet"></param>
+        /// <param name="pesonneId"></param>
         public void Detail(ref DataSet dataSet, Int32 pesonneId)
         {
             IDbCommand iDbCommand = CreateCommand("SELECT * FROM Personne WHERE PersonneId = @PersonneId");
@@ -21,6 +25,10 @@ namespace WebApplication1
             this.Fill(dataSet, "Personne", iDbCommand);
         }
 
+        /// <summary>
+        /// Donne la liste de toutes les personnes
+        /// </summary>
+        /// <param name="dataSet"></param>
         public void List(ref DataSet dataSet)
         {
             IDbCommand iDbCommand = CreateCommand("SELECT * FROM Personne");
@@ -28,6 +36,11 @@ namespace WebApplication1
             this.Fill(dataSet, "Personne", iDbCommand);
         }
 
+        /// <summary>
+        /// Recherche une personne sur base du nom
+        /// </summary>
+        /// <param name="dataSet"></param>
+        /// <param name="nom"></param>
         public void Search(ref DataSet dataSet, string nom)
         {
             IDbCommand iDbCommand = CreateCommand("SELECT * FROM Personne WHERE Nom LIKE @Nom");
@@ -36,10 +49,15 @@ namespace WebApplication1
             this.Fill(dataSet, "Personne", iDbCommand);
         }
 
+        /// <summary>
+        /// Sauvegarde des records
+        /// </summary>
+        /// <param name="dataSet"></param>
         public void Save(ref DataSet dataSet)
         {
         }
 
+        //Suppression du record
         public void Delete(Int32 pesonneId)
         {
             IDbCommand iDbCommand = CreateCommand("DELETE Personne WHERE PersonneId = @PersonneId");
